85b16a29a9445ee30411dc5c4dc3dbb829df2c88
Tanishq Mohod <88834766+Tanishq67m@users.noreply.github.com>
2024-01-25 00:09:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const slider = document.querySelector("[data-slider]");
+
+const track = slider.querySelector("[data-slider-track]");
+const prev = slider.querySelector("[data-slider-prev]");
+const next = slider.querySelector("[data-slider-next]");
+
+if (track) {
+  prev.addEventListener("click", () => {
+    next.removeAttribute("disabled");
+
+    track.scrollTo({
+      left: track.scrollLeft - track.firstElementChild.offsetWidth,
+      behavior: "smooth"
+    });
+  });
+
+  next.addEventListener("click", () => {
+    prev.removeAttribute("disabled");
+
+    track.scrollTo({
+      left: track.scrollLeft + track.firstElementChild.offsetWidth,
+      behavior: "smooth"
+    });
+  });
+
+  track.addEventListener("scroll", () => {
+    const trackScrollWidth = track.scrollWidth;
+    const trackOuterWidth = track.clientWidth;
+
+    prev.removeAttribute("disabled");
+    next.removeAttribute("disabled");
+
+    if (track.scrollLeft <= 0) {
+      prev.setAttribute("disabled", "");
+    }
+
+    if (track.scrollLeft === trackScrollWidth - trackOuterWidth) {
+      next.setAttribute("disabled", "");
+    }
+  });
+}
